d270c4d0491f4bd657e2ba18a36f5cbacad5c1a9
Stanislav Hmelevsky <s.v.hmelevsky@gmail.com>
2021-05-24 23:06:15 +0400

    Добавил метод all, который ждёт все promice, например сбор данных с разных endpoint на back-end

--- Promise/sleep.js
+++ Promise/sleep.js
@@ -6,5 +6,11 @@ const sleep = ms => {
     })
 }
 
-sleep(2000).then(() => console.log("After 2 sec"));
-sleep(5000).then(() => console.log("After 5 sec"));
+// sleep(2000).then(() => console.log("After 2 sec"));
+// sleep(5000).then(() => console.log("After 5 sec"));
+
+Promise
+.all([sleep(2000), sleep(5000)])
+.then(() => {
+    console.log("All promices");
+})
