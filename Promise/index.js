7ca8f5bba760cbad5841b3061766765f5dd38f1e
Stanislav Hmelevsky <s.v.hmelevsky@gmail.com>
2021-05-24 22:55:47 +0400

    Добавил catch и finally

--- Promise/index.js
+++ Promise/index.js
@@ -14,16 +14,22 @@ const promise = new Promise((resolve, reject) => {
 	}, 2000);
 });
 
-promise.then(data => {
+promise
+.then(data => {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
 			data.modified = true;
-			resolve(data);
+			reject(data);
 		}, 2000);
 	});
-}).then((clientData) => {
+})
+.then((clientData) => {
+	console.log("Base client data", clientData);
 	clientData.fromPromice = true;
 	return clientData;
-}).then((data) => {
-	console.log("Chain promice data", data);
-});
+})
+.then((data) => {
+	console.log("Modified chain promice data", data);
+})
+.catch(error => console.log("Error: ", error))
+.finally(() => console.log("Finally"));
