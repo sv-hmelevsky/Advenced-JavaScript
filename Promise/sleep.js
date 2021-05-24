e1d2011de0dcc777b6911d36d0e9bda349c4642d
Stanislav Hmelevsky <s.v.hmelevsky@gmail.com>
2021-05-24 23:08:05 +0400

    Добавил метод reace, какой из promice выполнится быстрее

--- Promise/sleep.js
+++ Promise/sleep.js
@@ -13,4 +13,10 @@ Promise
 .all([sleep(2000), sleep(5000)])
 .then(() => {
     console.log("All promices");
-})
+});
+
+Promise
+.race([sleep(2000), sleep(5000)])
+.then(() => {
+    console.log("Reace promices");
+});
