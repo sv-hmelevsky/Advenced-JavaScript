66aa0fe7dd6587ea4583417f653b0e4b31b7d92f
Stanislav Hmelevsky <s.v.hmelevsky@gmail.com>
2021-05-24 22:26:43 +0400

    Добавил promise

--- Promise/index.js
+++ Promise/index.js
@@ -1,16 +1,23 @@
 console.log("Request data...");
 
-setTimeout(() => {
-	console.log("Preparing data...");
-	
-	const requestedData = {
-		server: "aws",
-		port: 2000,
-		status: true
-	};
-	
+const promise = new Promise((resolve, reject) => {
 	setTimeout(() => {
-		requestedData.modified = true;
-		console.log("Data received", requestedData);
+		console.log("Preparing data...");
+		
+		const requestedData = {
+			server: "aws",
+			port: 2000,
+			status: true
+		};
+		
+		setTimeout(() => {
+			requestedData.modified = true;
+			console.log("Data received", requestedData);
+			resolve();
+		}, 2000);
 	}, 2000);
-}, 2000);
+});
+
+promise.then(() => {
+	console.log("Promise resolve!");
+});
