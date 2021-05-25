bd0b8037e0ad8fd828b0a47c0e4f0e7a33257750
Stanislav Hmelevsky <s.v.hmelevsky@gmail.com>
2021-05-25 23:17:30 +0400

    Заменил код promise на async\await

--- Async-Await/index.js
+++ Async-Await/index.js
@@ -1,11 +1,14 @@
 global.fetch = require("node-fetch");
 
 const jsonDataUrl = "https://jsonplaceholder.typicode.com/todos/1";
+
+// Задержка, имулирующая сервер
 const delay = (ms) => {
     // r сокращение от resolve, может называться как угодно
     return new Promise(r => setTimeout(() => r(), ms));
 }
 
+/* Вариант без Async и Await
 function fetchTodos() {
     console.log("Fetch todo's data...");
     return delay(2000)
@@ -20,3 +23,24 @@ fetchTodos()
     .catch(error => {
         console.error("Error: ", error);
     });
+*/
+
+// По сути данный код повторяет код выше, без лишнего награмождения колбэками
+// И выглядит как простой, синхронный код)) Т.к. Async\Await это синт. сахар над promise
+async function fetchAsyncTodos() {
+    try {
+        console.log("Fetch todo's data...");
+
+        await delay(2000);
+        const response = await fetch(jsonDataUrl);
+        const data = await response.json();
+
+        console.log("Data: ", data);
+    } catch(e) {
+        console.error("Error: ", e);
+    } finally {
+        console.info("Finally always here!))");
+    }
+};
+
+fetchAsyncTodos();
